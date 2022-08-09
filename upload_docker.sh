bc4840e4047714be6de15753d66577c97aed0993
EC2 Default User <ec2-user@ip-172-31-4-236.us-east-2.compute.internal>
2022-08-09 22:42:46 +0000

    upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,16 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
+
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=shaovafest/ml_app
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag ml_app $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
